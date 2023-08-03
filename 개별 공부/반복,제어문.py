42cdee3c769c4996b586cb0b1a26c2234282ab4c
RadWolf <38929244+Rad-Wolf@users.noreply.github.com>
2023-08-03 21:34:04 +0900

    1

---
+++ 개별 공부/반복,제어문.py
@@ -0,0 +1,34 @@
+# weather = input("오늘 날씨는 어때요?")
+
+# if weather =="비":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물이 필요 없어요")
+
+# temp = 0
+# for day in range(1,5):
+#     temp = int(input(f"{day}일차 기온은 어때요? "))
+#     if 30 <= temp :
+#         print("쪄죽을 수 있어요. 조심하세요")
+#     elif 20 <= temp :
+#         print("돌아다니기 좋은 날씨에요")
+#     elif 0 <= temp :
+#         print("쌀쌀 하네요 외투나 겉옷을 챙기세요")
+#     else:
+#         print("이불 밖은 위험 할 수 있어요 따듯한 곳을 찾으세요")
+
+
+# while
+
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print(f"{customer}님, 커피가 준비되었습니다.")
+#     index -= 1
+#     if index == 0:
+#         print(f"{customer}님의 커피는 폐기 됩니다.")
+
+# continu, brack
+
