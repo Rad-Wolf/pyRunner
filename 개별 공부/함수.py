6ca44b134a82283333a134f9629ccf7b33cf6fa7
RadWolf <38929244+Rad-Wolf@users.noreply.github.com>
2023-08-04 20:33:53 +0900

    1

--- 개별 공부/함수.py
+++ 개별 공부/함수.py
@@ -53,21 +53,41 @@
 # numbering(10,20)
 
 # 지역변수 전역변수
-gun = 10
 
-def checkpoint(soldiers): # 경계근무
-    global gun # 전역 공간에 있는(위에 있는) gun 을 사용할 수 있음
-    gun = gun - soldiers # 오류 -> 여기서 사용하는 것은 지역에서만 사용 가능한 gun임!
-                         #         이전에 사용할 수 있는 변수를 따로 만들어줘야함
-    print("함수 내 남은 총 {0}".format(gun))
+# gun = 10
 
-def checkpoint_return(gun, soldiers): # 가장 이상적인 방법? 전역변수는 가급적 적게 쓰는게 좋음
-    gun = gun - soldiers
-    print(f"함수 내 남은 총 : {gun}")
-    return gun
+# def checkpoint(soldiers): # 경계근무
+#     global gun # 전역 공간에 있는(위에 있는) gun 을 사용할 수 있음
+#     gun = gun - soldiers # 오류 -> 여기서 사용하는 것은 지역에서만 사용 가능한 gun임!
+#                          #         이전에 사용할 수 있는 변수를 따로 만들어줘야함
+#     print("함수 내 남은 총 {0}".format(gun))
 
-print(f"전체 총 : {gun}")
-checkpoint(2) # 2명이 근무를 나감
-print(f"전체 총 : {gun}")
-gun = checkpoint_return(gun,2) # 2명 복귀
-print(f"전체 총 : {gun}")
+# def checkpoint_return(gun, soldiers): # 가장 이상적인 방법? 전역변수는 가급적 적게 쓰는게 좋음
+#     gun = gun - soldiers
+#     print(f"함수 내 남은 총 : {gun}")
+#     return gun
+
+# print(f"전체 총 : {gun}")
+# checkpoint(2) # 2명이 근무를 나감
+# print(f"전체 총 : {gun}")
+# gun = checkpoint_return(gun,2) # 2명 복귀
+# print(f"전체 총 : {gun}")
+
+# 문제 : 표준체중
+# 남 : 키(m)X 키(m) X 22
+# 여 : 키(m)X 키(m) X 21
+# 함수명 : std_weight(height,sex)
+# 표준 체중은 소수점 둘재자리까지
+
+def std_seight(height:int,sex:str):
+    if sex == "남":
+        print(f"남성 키 {height}cm에 따른 표준 체중 : {height * 0.01 * height * 0.01 * 21:.2f}kg")
+    elif sex == "여":
+        print(f"여성 키 {height}cm에 따른 표준 체중 : {height * 0.01 * height * 0.01 * 21:.2f}kg")
+# 소수점 표현방식
+# 1. round(수,자리수)
+# 2. 포멧팅 :.2f (두자리 수까지)
+
+std_seight(177,"남")
+std_seight(152,"여")
+std_seight(162,"남")
