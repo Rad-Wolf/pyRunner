821dfef20a8dca15194f552bbc557a7883eb03a2
RadWolf <38929244+Rad-Wolf@users.noreply.github.com>
2023-08-14 20:38:42 +0900

    11

--- 개별 공부/Class.py
+++ 개별 공부/Class.py
@@ -64,34 +64,72 @@
 
 # pass super
 
-class Unit:
-    def __init__(self,name,hp):
-        self.name = name
-        self.hp = hp
-    def Move(self,location):
-        print("[유닛 이동]")
+# class Unit:
+#     def __init__(self,name,hp):
+#         self.name = name
+#         self.hp = hp
+#     def Move(self,location):
+#         print("[유닛 이동]")
 
-class GraundUnit(Unit):
-    def __init__(self,name,hp,move_speed):
-        Unit.__init__(self,name,hp)
-        self.move_speed = move_speed
-    def Move(self,location):
-        Unit.Move(self,location)
-        print(f"{self.name} : {location} 방향으로 지상 이동")
+# class GraundUnit(Unit):
+#     def __init__(self,name,hp,move_speed):
+#         Unit.__init__(self,name,hp)
+#         self.move_speed = move_speed
+#     def Move(self,location):
+#         Unit.Move(self,location)
+#         print(f"{self.name} : {location} 방향으로 지상 이동")
 
-class FlyableUnit(Unit):
-    def __init__ (self,name,hp,Flying_speed):
-        Unit.__init__(self,name,hp)
-        self.Flying_speed
-    def Move(self,location):
-        Unit.Move(self,location)
-        print(f"{self.name} : {location} 방향으로 공중 이동")
+# class FlyableUnit(Unit):
+#     def __init__ (self,name,hp,Flying_speed):
+#         Unit.__init__(self,name,hp)
+#         self.Flying_speed
+#     def Move(self,location):
+#         Unit.Move(self,location)
+#         print(f"{self.name} : {location} 방향으로 공중 이동")
 
-# pass
-class building_Uni(Unit):
-    def __init__(self,name,hp,location):
-        pass # 우선 넘어감 -> 매서드를 바로 return 시킴?
-    def Move(self,location):
-        super().move(location) # Unit 대신 super로 바꿧는데 상위 클래스의 메서드 등을 쓸 수 있게 함
-        print("super()") # 다중상속의 경우 super를 썼다면? : 마지막에 써있는 상속 클레스를 의미하게 됨 # 다중 상속시 명시를 하는게 좋다.
+# # pass
+# class building_Uni(Unit):
+#     def __init__(self,name,hp,location):
+#         pass # 우선 넘어감 -> 매서드를 바로 return 시킴?
+#     def Move(self,location):
+#         super().move(location) # Unit 대신 super로 바꿧는데 상위 클래스의 메서드 등을 쓸 수 있게 함
+#         print("super()") # 다중상속의 경우 super를 썼다면? : 마지막에 써있는 상속 클레스를 의미하게 됨 # 다중 상속시 명시를 하는게 좋다.
         
+# 퀴즈 주어진 코드를 사용하여 부동산 프로그램을 작성하시오.
+
+# 3개의 매물 
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 전세 5억 2007년
+# 송파 빌라 월세 500/50 2000년
+
+#[코드]
+# class House:
+#     # 매물 초기화
+#     def __init__(self,location,house_type,deal_type,price,completion_year):
+#         pass
+#     # 매물 정보 표시
+#     def show_detail(self):
+#         pass
+
+class House:
+    def __init__(self,location,house_type,deal_type,price,completion_year):
+        self.location : str = location
+        self.house_type : str = house_type
+        self.deal_type : str = deal_type
+        self.price : str = price
+        self.completion_year : str = completion_year
+        print("매물 등록 완료")
+
+    def show_detail(self):
+        string = self.deal_type.ljust(6)+"\n"+ \
+                 "[위치 : " + self.location.rjust(10) + "] [주거형태 : " + self.house_type.rjust(5)+"] [가격 : " + self.price.rjust(10) + "] [준공년도 : " + self.completion_year.rjust(5)+"]"
+        print(string)
+
+houses = []
+houses.append(House("강남","아파트","매매","10억","2010년"))
+houses.append(House("마포","오피스텔","전세","5억","2007년"))
+houses.append(House("송파","빌라","월세","500/50","2000년"))
+
+print(f"총 {str(len(houses)).rjust(3)}대의 매물이 있습니다.")
+for house in houses:
+    house.show_detail()
