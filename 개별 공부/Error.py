c04dd25048a8ff9a7891f0fa20c8f59bf215cdd2
RadWolf <38929244+Rad-Wolf@users.noreply.github.com>
2023-08-21 14:15:47 +0900

    1

--- 개별 공부/Error.py
+++ 개별 공부/Error.py
@@ -5,6 +5,8 @@ try:
     print(f"{num1}/{num2} = {int(num1/num2)}")
 except ValueError:
     print("잘못된 값을 입력하였습니다.")
-except ZeroDivisionError:
-    print("0 에러")
+except ZeroDivisionError as err: # 변수로 나오게 할 수도 있음
+    print(f"{err} 0 에러")
+except :
+    print("알 수 없는 에러가 발생했습니다.")
 # 등등등의 에러를 표시할 수 있다.
